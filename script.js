891d761c3a602a3fb4c6d983e48c5e80e6fc7567
Hatelyn <91886259+Hatelyn@users.noreply.github.com>
2024-01-22 02:52:58 +0100

    script
    script

---
+++ script.js
@@ -0,0 +1,31 @@
+function start() {
+    let nav = `<ul>`
+
+    resources.map(element => nav += `<li id="${element.category}" onClick="navClick('${element.category}')">${element.category}</li>`) 
+
+    nav += `</ul>`
+
+    document.getElementById("nav").innerHTML = nav
+}
+
+start()
+
+function navClick(clicked) {
+    start()
+    
+    let content = ""
+    let filteredCat = resources.filter(filter => filter.category === clicked)
+
+
+    filteredCat.map(e => {
+        content += `<article><h1>${e.category}</h1><p>${e.text}</p><ul>`
+
+        let open = document.getElementById(`${e.category}`)
+        open.classList.toggle("clicked")
+
+        e.sources.map(link => content += `<li><a class="title" href="${link.url}">${link.title}</a></li>`)
+
+        content += `</ul></article>`
+    })
+    document.getElementById("content").innerHTML = content
+}
